90a05a0fc1d92c337b539b42a26126dc17ed13c3
danishrahmankpm <140050542+danishrahmankpm@users.noreply.github.com>
2025-06-17 22:38:29 +0530

    updates

--- qeatsbackend/src/main/java/com/crio/qeats/repositories/RestaurantRepository.java
+++ qeatsbackend/src/main/java/com/crio/qeats/repositories/RestaurantRepository.java
@@ -12,6 +12,6 @@ import java.util.Optional;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
-public interface RestaurantRepository extends MongoRepository<RestaurantEntity, String>, RestaurantRepositoryCustom {
+public interface RestaurantRepository extends MongoRepository<RestaurantEntity, String>{
 }
 
